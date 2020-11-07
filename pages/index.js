9fa1e23d7d44e6259b861ec172cf9bb23640b331
AGuyNextDoor <martin.vielvoye@gmail.com>
2020-11-07 20:53:41 +0100

    added homepage

--- pages/index.js
+++ pages/index.js
@@ -6,29 +6,42 @@ import { StaticRouter } from "react-router-dom";
 // import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export default class extends Component {
-  render() {
-    return(
-      <StaticRouter>
-        <App />
-      </StaticRouter>
-    )
-  }
+const HomePage = () => {
+
+  return (
+    <>
+      <h1>
+        Welcome to <text class="font-weight-bold">a-blog-nextdoor</text>
+      </h1>
+      <br />
+      <text class="font-weight-light" name="homepage" id="homepage">
+        <p>This blog is a journal of somebody having fun with too many topics at the same time.</p>
+        <p>Enjoy your stay! Don't hesitate to contact me for any information.</p>
+      </text>
+      <br />
+      <br />
+      <hr />
+      <text class="font-italic">Music of the moment</text>: Ball and Biscuit - The White Stripes
+      <br />
+      <br />
+      <footer class="font-weight-light bg-light">
+        <text class="font-weight-normal">Contact information:</text>
+        <br />
+        <li>
+          <a href="https://a-blog-nextdoor.vercel.app/Reflections/ChipsStick.md">Personal GitHub</a>
+        </li>
+        <li>
+          <a href="https://github.com/AGuyNextDoor/a-blog-nextdoor">Blog's GitHub</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/martinvielvoye/">Linkedin</a>
+        </li>
+        <li>
+          <a href="mailto:contact.aguynextdoor@gmail.com">Mail</a>
+        </li>
+      </footer>
+    </>
+  );
 }
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>,
-//   document.getElementById("root"),
-// );
-
-
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+export default HomePage
