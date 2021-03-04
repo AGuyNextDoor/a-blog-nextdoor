018ec09f86dd836ae18ade59ba17e8dc70907dcf
Martin Vielvoye <martin.vielvoye@gmail.com>
2021-03-04 14:25:10 +0100

    trying loading page

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,21 @@ import React from "react";
 import Navbar from "../components/navbar";
 import Sidebar from "../components/sidebar";
 import Head from "next/head";
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import "nprogress/nprogress.css";
 import './index.css'
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: 'ease',
+  speed: 800,
+  showSpinner: false,
+});
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
