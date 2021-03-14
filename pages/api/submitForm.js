e873e1573833f172952ab8dfdfe1abff3a77d4c4
AGuyNextDoor <martin.vielvoye@gmail.com>
2021-03-14 17:58:49 +0100

    corrected redirection message from api

--- pages/api/submitForm.js
+++ pages/api/submitForm.js
@@ -7,27 +7,14 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     let result = await addVotes(req.body)
 
-    console.log({result});
-
     const urlDiscussionID = String("/Turing_Judges/Forms/"+encodeURIComponent(req.body.discussion_id))
-    
-    let flag = false
 
     if(result === "Votes Disabled"){
       res.redirect(urlDiscussionID +"?e="+encodeURIComponent("Votes are disabled"))
     } else {
-
-      if(result.result.n === 1){
-        flag = true;
-      }
-      
-      if(flag){
-        res.redirect(urlDiscussionID+"?e="+encodeURIComponent("Error no answer submitted contact host"))
-      } else {
-        res.redirect(urlDiscussionID+"?m="+encodeURIComponent("Thank you!"))
-      }
+      res.redirect(urlDiscussionID+"?m="+encodeURIComponent("Thank you!"))
     }
    } else {
-      res.redirect(urlDiscussionID+"?e="+encodeURIComponent("Error no answer submitted contact host"))
+      res.redirect(urlDiscussionID+"?e="+encodeURIComponent("Request Not a POST"))
     }
 }
