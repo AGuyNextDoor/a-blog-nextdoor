1800103177f4ba310b8c3770b727c6b7676fb8ad
Martin Vielvoye <martin.vielvoye@gmail.com>
2023-10-19 10:16:54 +0200

    update

--- pages/_app.js
+++ pages/_app.js
@@ -61,7 +61,7 @@ export default function MyApp({ Component, pageProps }) {
         {/* <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script> */}
       </body>
     </Provider>
-    < Analytics / >
+    <Analytics />
   </>
   );
 }
